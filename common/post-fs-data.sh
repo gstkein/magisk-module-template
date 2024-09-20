d7c2fb55e222adbd3d8eda3cb8c7405c0dbacdf2
Guillermo Steren <gstkein@gmail.com>
2024-09-20 17:29:38 -0300

    initial

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,3 +5,19 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
+# Path to the original APK
+ORIG_APK="/system/priv-app/TabUILenovoLauncher/TabUILenovoLauncher.apk"
+
+# Path to your custom APK
+NEW_APK="/data/adb/modules_update/tabui_launcher_replacement/system/priv-app/TabUILenovoLauncher/TabUILenovoLauncher12.apk"
+
+# Check if the original APK exists
+if [ -f "$ORIG_APK" ]; then
+    # Bind mount the custom APK over the original APK
+    mount --bind "$NEW_APK" "$ORIG_APK"
+fi
+
+# Ensure correct permissions for the new APK
+chmod 644 "$NEW_APK"
+chown root:root "$NEW_APK"
